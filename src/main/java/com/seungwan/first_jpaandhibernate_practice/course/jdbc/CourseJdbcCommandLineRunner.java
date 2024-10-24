68c089bc37de21d0a7889866260e6fde47494453
Programming-Seungwan <swsj4480@gmail.com>
2024-10-24 14:07:11 +0900

    Feat : first use of jdbc
    1. CourseJdbcRepository 클래스를 만들어주고 springJdbcTemplate을 자오 주입해줌 그리고 삽입 명령어를 만듬
    2. 처음 애플리케이션이 실행될 때 러닝을 위한 commandlineRunner를 또 만들어줌

---
+++ src/main/java/com/seungwan/first_jpaandhibernate_practice/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,17 @@
+package com.seungwan.first_jpaandhibernate_practice.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert();
+    }
+}
---
+++ src/main/java/com/seungwan/first_jpaandhibernate_practice/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,20 @@
+package com.seungwan.first_jpaandhibernate_practice.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate  springJdbcTemplate;
+
+    private static String INSERT_QUERY=
+
+            """
+             insert into course (id,name,author) values(1, 'learn aws', 'seungwanTeach');       """;
+
+    public void insert() {
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+}
