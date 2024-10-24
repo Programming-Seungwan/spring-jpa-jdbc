d0e1ae865a881753e09f30535285ed3d5efea2bb
Programming-Seungwan <swsj4480@gmail.com>
2024-10-24 13:24:16 +0900

    Feat : first h2 database config
    1. h2의 관리 콘솔을 h2-console url에서 실행할 수 있도록 설정함

---
+++ src/main/java/com/seungwan/first_jpaandhibernate_practice/FirstJpaandhibernatePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.seungwan.first_jpaandhibernate_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJpaandhibernatePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJpaandhibernatePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungwan/first_jpaandhibernate_practice/FirstJpaandhibernatePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungwan.first_jpaandhibernate_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJpaandhibernatePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
